49ebd02eba0b42a03c52f9e7be0909b0b5557369
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-06-05 10:40:23 -0300

    Add return button to settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 class Settings extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <header>
           <h2 data-testid="settings-title">Settings</h2>
           <p>Still is progress...</p>
         </header>
+        <button
+          type="button"
+          className="back-btn"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Back to Start
+        </button>
       </div>
     );
   }
